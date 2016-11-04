8bbe4cc0c242afd4c49d4b8c551fc7a5e277e4b9
MANISH-GIRI <manish.giri.me@gmail.com>
2016-11-04 04:07:39 -0400

    Complete main class for using OrderedArray class

--- Java/Data-Structures/Arrays/Binary-Search/src/OrderedApp.java
+++ Java/Data-Structures/Arrays/Binary-Search/src/OrderedApp.java
@@ -17,6 +17,46 @@ public class OrderedApp {
         orderedArray.insert(60);
         orderedArray.insert(70);
         orderedArray.insert(80);
-        orderedArray.insert(20);
+        orderedArray.insert(90);
+        orderedArray.insert(100);
+
+        System.out.println("finished inserting items. Current elements - ");
+        orderedArray.display();
+
+        //search for an item
+        int searchKey = 55;
+        int pos = orderedArray.find(searchKey);
+        if(pos != orderedArray.size()) {
+            System.out.println("Found search key " + searchKey + " at position " + pos);
+        }
+        else {
+            System.out.println("Search key " + searchKey + " was not found.");
+        }
+
+        orderedArray.display();
+
+        //delete 3 items
+        if(orderedArray.delete(40)) {
+            System.out.println("40 was successfully deleted");
+        }
+        else {
+            System.out.println("40 was not found in the array");
+        }
+        if(orderedArray.delete(55)) {
+            System.out.println("55 was successfully deleted");
+        }
+        else {
+            System.out.println("55 was not found in the array");
+        }
+        if(orderedArray.delete(90)) {
+            System.out.println("90 was successfully deleted");
+        }
+        else {
+            System.out.println("90 was not found in the array");
+        }
+
+        //display again
+        orderedArray.display();
+
     }
 }
